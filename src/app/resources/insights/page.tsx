84e1915ad2b35f8b747c0075a8807bdc9d9b23b2
Utkarsh <ukyp2014@gmail.com>
2024-04-15 12:03:54 +0530

    test commit

--- src/app/resources/insights/page.tsx
+++ src/app/resources/insights/page.tsx
@@ -38,8 +38,8 @@ export default function Insights() {
                   office—at least in one particular sector: retail. Last year,
                   women accounted for 35% of new CEO appointments at retail
                   companies, the highest ever, according to new report from Korn
-                  Ferry. That&apos;s up from 5% in 2012. "Adding women CEOs has been
-                  a constant theme of the last couple of years," says Sarah Lim,
+                  Ferry. That&apos;s up from 5% in 2012. &quot;Adding women CEOs has been
+                  a constant theme of the last couple of years,&quot; says Sarah Lim,
                   Korn Ferry&apos;s managing director of the Consumer Board and CEO
                   Services practice and author of the report.
                 </p>
@@ -53,8 +53,8 @@ export default function Insights() {
                   transitions, or retirements. However, that led to almost half
                   the new appointments in luxury/fashion businesses, including
                   department stores. That likely accounts for why a larger than
-                  usual portion of the new CEOs were women, Lim says. “Fashion
-                  tends to attract more women than DIY,” she says.
+                  usual portion of the new CEOs were women, Lim says. &ldquo;Fashion
+                  tends to attract more women than DIY,&rdquo; she says.
                 </p>
               </div>
               <div>
@@ -65,9 +65,9 @@ export default function Insights() {
                   decades, as budding executives need time and mentoring to get
                   experience in multiple roles across the business, says Donna
                   Herdsman, a Korn Ferry senior client partner and head of
-                  diversity, equity, and inclusion for EMEA in London. “These
+                  diversity, equity, and inclusion for EMEA in London. &ldquo;These
                   high-potential candidates need opportunities to demonstrate
-                  what they can achieve,” she says.
+                  what they can achieve,&rdquo; she says.
                 </p>
               </div>
               <div>
@@ -80,8 +80,8 @@ export default function Insights() {
                   their hiring, says Stuart Richards, Korn Ferry&apos;s sector
                   leader, consumer products in the UK and EMEA. Employers might
                   consider skills-based recruitment instead of hiring based on
-                  graduating from certain top colleges, Richards says. “Skills
-                  tell employers whether people can fundamentally do the job,”
+                  graduating from certain top colleges, Richards says. &ldquo;Skills
+                  tell employers whether people can fundamentally do the job,&rdquo;
                   he says.
                 </p>
               </div>
@@ -91,8 +91,8 @@ export default function Insights() {
                   sponsorship from a senior staff member to succeed. Such a
                   sponsor can advise the individual on which experiences or
                   roles to pursue. Perhaps most importantly, a mentor can help
-                  rising stars avoid trouble. “Even if you make a mistake that
-                  anyone could make, you need support,” she says.
+                  rising stars avoid trouble. &ldquo;Even if you make a mistake that
+                  anyone could make, you need support,&rdquo; she says.
                 </p>
               </div>
               <div>
@@ -100,8 +100,8 @@ export default function Insights() {
                   Ultimately, CEO choices rest with the board of directors. When
                   the lead independent director and the senior independent
                   director become women, the pendulum swings toward female CEO
-                  candidates, Lim says. “It becomes more natural for the board
-                  to consider women,” she says. “It happens over time.”
+                  candidates, Lim says. &ldquo;It becomes more natural for the board
+                  to consider women,&rdquo; she says. &ldquo;It happens over time.&rdquo;
                 </p>
               </div>
             </div>
