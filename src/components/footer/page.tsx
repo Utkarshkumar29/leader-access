1f89d248057f151ce1b385a1f9d72ff7e46ed2d7
Utkarsh <ukyp2014@gmail.com>
2024-04-15 18:00:42 +0530

    Sixth Commit

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,19 +1,19 @@
-import style from "../../styles/footer.module.css";
+import footerStyle from "../../styles/footer.module.css";
 import Image from "next/image";
 
 export default function Footer(){
     return(
-        <div className={style.footerContainer}>
-            <div className={style.footerWrapper}>
-                <div className={style.footerContents}>
-                    <div className={style.whiteContainer}>
+        <div className={footerStyle.footerContainer}>
+            <div className={footerStyle.footerWrapper}>
+                <div className={footerStyle.footerContents}>
+                    <div className={footerStyle.whiteContainer}>
                         <Image src="/images/landing/white_logo.svg" alt="Error" width={40} height={40}/>
-                        <p className={style.footerHeading}>Leaders Access</p>
+                        <p className={footerStyle.footerHeading}>Leaders Access</p>
                     </div>
-                    <div className={style.linksWrapper}>
-                        <div className={style.topicsContainer}>
-                            <p className={style.topicsHeading}>Pages</p>
-                            <div className={style.topics}>
+                    <div className={footerStyle.linksWrapper}>
+                        <div className={footerStyle.topicsContainer}>
+                            <p className={footerStyle.topicsHeading}>Pages</p>
+                            <div className={footerStyle.topics}>
                                 <p>Home</p>
                                 <p>About</p>
                                 <p>Insights</p>
@@ -21,35 +21,35 @@ export default function Footer(){
                                 <p>Contact</p>
                             </div>
                         </div>
-                        <div className={style.topicsContainer}>
-                            <p className={style.topicsHeading}>Services</p>
-                            <div className={style.topics}>
+                        <div className={footerStyle.topicsContainer}>
+                            <p className={footerStyle.topicsHeading}>Services</p>
+                            <div className={footerStyle.topics}>
                                 <p>Executive Search</p>
                                 <p>Talent Advisory</p>
                                 <p>Recruitment Process</p>
                                 <p>Outsourcing</p>
                             </div>
                         </div>
-                        <div className={style.topicsContainer}>
-                            <p className={style.topicsHeading}>Expertise</p>
-                            <div className={style.topics}>
+                        <div className={footerStyle.topicsContainer}>
+                            <p className={footerStyle.topicsHeading}>Expertise</p>
+                            <div className={footerStyle.topics}>
                                 <p>Industries</p>
                                 <p>Functions</p>
                             </div>
                         </div>
-                        <div className={style.topicsContainer}>
-                            <p className={style.topicsHeading}>Jobs</p>
-                            <div className={style.topics}>
+                        <div className={footerStyle.topicsContainer}>
+                            <p className={footerStyle.topicsHeading}>Jobs</p>
+                            <div className={footerStyle.topics}>
                                 <p>Browse Jobs</p>
                                 <p>Submit Your CV</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className={style.whiteLine}></div>
-                    <div className={style.copyRight}>
+                <div className={footerStyle.whiteLine}></div>
+                    <div className={footerStyle.copyRight}>
                     <p>Copyright © 2024 Leaders Access</p>
-                    <div className={style.terms}>
+                    <div className={footerStyle.terms}>
                         <p>Privacy Policy</p>
                         <p>Terms & Conditions</p>
                         <p>Disclaimer</p>
